8f2e10d4e904f36e389ae45c933c2b25eda2f02e
Charles Stover <github@charlesstover.com>
2018-08-24 14:23:58 -0500

    line breaks

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,13 +3,11 @@
 if (
   array_key_exists('first_name', $_POST) &&
   !preg_match('/^\s*$/', $_POST['first_name'])
-)
-{
+) {
   if (
     array_key_exists('last_name', $_POST) &&
     !preg_match('/^\s*$/', $_POST['last_name'])
-  )
-  {
+  ) {
     if (
       array_key_exists('phone_ac', $_POST) &&
       array_key_exists('phone_end', $_POST) &&
@@ -17,15 +15,12 @@ if (
       preg_match('/^\d{3}$/', $_POST['phone_ac']) &&
       preg_match('/^\d{4}$/', $_POST['phone_end']) &&
       preg_match('/^\d{3}$/', $_POST['phone_mid'])
-    )
-    {
+    ) {
       if (
         array_key_exists('email', $_POST) &&
         preg_match('/^.+\@.+\..{2,8}$/', $_POST['email'])
-      )
-      {
-        if (array_key_exists('unsecured_debt', $_POST))
-        {
+      ) {
+        if (array_key_exists('unsecured_debt', $_POST)) {
           $_POST['first_name'] = str_replace(Array("\n", "\r"), '', $_POST['first_name']);
           $_POST['last_name'] = str_replace(Array("\n", "\r"), '', $_POST['last_name']);
           $_POST['email'] = str_replace(Array("\n", "\r"), '', $_POST['email']);
@@ -56,4 +51,5 @@ if (
 }
 else
   header('Location: index.html?error=1');
+
 ?>
